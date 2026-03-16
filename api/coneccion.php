0a660a7612b31482ba72be4a112190fe208378cb
mitzate <mitzate@gmail.com>
2026-03-16 01:23:55 -0700

    Modulos

--- api/coneccion.php
+++ api/coneccion.php
@@ -304,20 +304,24 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         try {
             // Construir la consulta combinada (UNION) y aplicar filtros sobre el resultado
             $sql = "SELECT * FROM (\n";
-            $sql .= "  SELECT id_caida AS id, id_dispositivo, fecha_hora, 'caidas' AS origen,\n";
-            $sql .= "         presencia, movimiento AS informacion_movimiento, parametro_movimiento,\n";
+            $sql .= "  SELECT sc.id_caida AS id, sc.id_dispositivo, sc.fecha_hora, 'caidas' AS origen,\n";
+            $sql .= "         d.nombre_identificador AS nombre_identificador, d.ubicacion_lugar,\n";
+            $sql .= "         sc.presencia, sc.movimiento AS informacion_movimiento, sc.parametro_movimiento,\n";
             $sql .= "         NULL AS tasa_respiracion, NULL AS Frecuencia_cardiaca,\n";
             $sql .= "         NULL AS en_cama, NULL AS estado_sueno, NULL AS calidad_sueno_puntaje,\n";
             $sql .= "         NULL AS duracion_sueno_profundo, NULL AS duracion_despierto, NULL AS numero_vueltas\n";
-            $sql .= "    FROM t_sensores_caidas\n";
+            $sql .= "    FROM t_sensores_caidas sc\n";
+            $sql .= "    LEFT JOIN t_dispositivos d ON sc.id_dispositivo = d.id_dispositivo\n";
             $sql .= "  UNION ALL\n";
-            $sql .= "  SELECT id_sueno AS id, id_dispositivo, fecha_hora, 'sueno' AS origen,\n";
+            $sql .= "  SELECT ss.id_sueno AS id, ss.id_dispositivo, ss.fecha_hora, 'sueno' AS origen,\n";
+            $sql .= "         d.nombre_identificador AS nombre_identificador, d.ubicacion_lugar,\n";
             $sql .= "         NULL AS presencia, NULL AS informacion_movimiento, NULL AS parametro_movimiento,\n";
-            $sql .= "         frecuencia_respiratoria_promedio AS tasa_respiracion,\n";
-            $sql .= "         frecuencia_cardiaca_promedio AS Frecuencia_cardiaca,\n";
-            $sql .= "         en_cama, estado_sueno, calidad_sueno_puntaje,\n";
-            $sql .= "         duracion_sueno_profundo, duracion_despierto, numero_vueltas\n";
-            $sql .= "    FROM t_sensores_sueño\n";
+            $sql .= "         ss.frecuencia_respiratoria_promedio AS tasa_respiracion,\n";
+            $sql .= "         ss.frecuencia_cardiaca_promedio AS Frecuencia_cardiaca,\n";
+            $sql .= "         ss.en_cama, ss.estado_sueno, ss.calidad_sueno_puntaje,\n";
+            $sql .= "         ss.duracion_sueno_profundo, ss.duracion_despierto, ss.numero_vueltas\n";
+            $sql .= "    FROM t_sensores_sueño ss\n";
+            $sql .= "    LEFT JOIN t_dispositivos d ON ss.id_dispositivo = d.id_dispositivo\n";
             $sql .= ") AS combined";
 
             $conditions = [];
